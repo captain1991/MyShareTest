9a047448521d64cbd59050100c6f63200543e4aa
captain1991 <m18255031805_1@163.com>
2017-11-07 21:11:01 +0800

    update

---
+++ tu8me/src/main/java/com/xiaodong/tu8me/TestActivity.java
@@ -0,0 +1,20 @@
+package com.xiaodong.tu8me;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+    public void onClick(View view){
+
+    }
+
+}
